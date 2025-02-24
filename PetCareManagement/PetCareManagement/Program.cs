2e943b0781e92ca9c56cbf5b1cefe8a9eeb9daba
Nahida_22 <nr681@live.mdx.ac.uk>
2025-02-24 13:07:23 +0400

    intial commit

---
+++ PetCareManagement/PetCareManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PetCareManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
