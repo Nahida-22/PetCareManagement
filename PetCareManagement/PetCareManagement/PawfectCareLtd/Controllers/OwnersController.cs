86d15dbb65773e2e5c0edb3a44f75cfcd792d5c9
Nahida_22 <nr681@live.mdx.ac.uk>
2025-03-07 11:50:36 +0400

    Connection to database and creation of owner table

---
+++ PetCareManagement/PetCareManagement/PawfectCareLtd/Controllers/OwnersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OwnersController : ControllerBase
+{
+    private readonly CsvReaderService _csvReaderService;
+
+    public OwnersController(CsvReaderService csvReaderService)
+    {
+        _csvReaderService = csvReaderService;
+    }
+
+    // Endpoint to load Owners data from a CSV file
+    [HttpPost("load-owners-csv")]
+    public IActionResult LoadOwnersCsv()
+    {
+        // Path to your CSV file (you can also pass the path dynamically)
+        var filePath = "C:\\Users\\23052\\Desktop\\Coursework\\PetCareLtd\\PetCareManagement\\PetCareManagement\\PawfectCareLtd\\CSV\\Owner.csv";
+        _csvReaderService.ReadOwnersFromCsv(filePath);
+
+        return Ok("Owners data loaded successfully.");
+    }
+
+    
+    
+}
