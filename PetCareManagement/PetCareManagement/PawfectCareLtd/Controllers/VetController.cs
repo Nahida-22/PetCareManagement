3458c3657e285b7beb2859503fd250c4b09d48f5
bteena04 <teenabusgeeth04@hotmail.com>
2025-03-09 10:52:55 +0400

    Vet Class and Vet Context added

---
+++ PetCareManagement/PetCareManagement/PawfectCareLtd/Controllers/VetController.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+using PawfectCareLtd.Data;
+using PawfectCareLtd.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace PawfectCareLtd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class VetController : ControllerBase
+    {
+        private readonly VetContext vetContext;
+
+        public VetController(VetContext vetContext) 
+        {
+            this.vetContext = vetContext;
+        }
+
+        [HttpGet]
+        [Route ("GetVets")]
+        public List<Vet> GetVets()
+        {
+            return vetContext.Vet.ToList();
+        }
+
+        [HttpPost]
+        [Route ("AddVet")]
+
+        public string AddVet(Vet vet)
+        {
+            string response = string.Empty;
+            vetContext.Vet.Add(vet);
+            vetContext.SaveChanges();
+            return "Vet added";
+        }
+    }
+}
--- PetCareManagement/PetCareManagement/PawfectCareLtd/Data/DatabaseContext.cs
+++ PetCareManagement/PetCareManagement/PawfectCareLtd/Data/DatabaseContext.cs
@@ -1,23 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
 
-namespace PawfectCareLtd.Models
-{
+//namespace PawfectCareLtd.Models
+//{
    
-        public class DatabaseContext : DbContext
-        {
-            public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
-            public DbSet<Owner> Owners { get; set; }
-            public DbSet<Pet> Pets { get; set; }
-            public DbSet<Appointment> Appointments { get; set; }
-            public DbSet<Prescription> Prescriptions { get; set; }
-            public DbSet<Medication> Medications { get; set; }
-            public DbSet<Vet> Vets { get; set; }
-            public DbSet<Supplier> Suppliers { get; set; }
-            public DbSet<Order> orders { get; set; }
-            public DbSet<Location> Locations { get; set; }
+//        public class DatabaseContext : DbContext
+//        {
+//            public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+//            public DbSet<Owner> Owners { get; set; }
+//            public DbSet<Pet> Pets { get; set; }
+//            public DbSet<Appointment> Appointments { get; set; }
+//            public DbSet<Prescription> Prescriptions { get; set; }
+//            public DbSet<Medication> Medications { get; set; }
+//            public DbSet<Vet> Vets { get; set; }
+//            public DbSet<Supplier> Suppliers { get; set; }
+//            public DbSet<Order> orders { get; set; }
+//            public DbSet<Location> Locations { get; set; }
 
 
 
-        }
+//        }
     
-}
+//}
