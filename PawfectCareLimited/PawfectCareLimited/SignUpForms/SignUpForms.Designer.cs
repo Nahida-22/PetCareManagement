53c3e6ca2e352b0dd6b7f2c20ad57095148adfbb
APJB7 <pothanah.adrian@gmail.com>
2025-04-15 12:10:57 +0400

    Sign Up front end

---
+++ PawfectCareLimited/PawfectCareLimited/SignUpForms/SignUpForms.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace PawfectCareLimited(Winforms).SignUpForms
+{
+    partial class SignUpForms
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        pictureBox1 = new PictureBox();
+        pictureBox2 = new PictureBox();
+        pictureBox3 = new PictureBox();
+        pictureBox4 = new PictureBox();
+        pictureBox5 = new PictureBox();
+        linkLabel1 = new LinkLabel();
+        label1 = new Label();
+        button1 = new Button();
+        linkLabel2 = new LinkLabel();
+        panel1 = new Panel();
+        panel2 = new Panel();
+        panel3 = new Panel();
+        panel4 = new Panel();
+        textBox1 = new TextBox();
+        textBox2 = new TextBox();
+        textBox3 = new TextBox();
+        textBox4 = new TextBox();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+        SuspendLayout();
+        // 
+        // pictureBox1
+        // 
+        pictureBox1.Image = PawfectCareLimited_Winforms_.Resource1.image_7;
+        pictureBox1.Location = new Point(80, -34);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new Size(123, 113);
+        pictureBox1.TabIndex = 0;
+        pictureBox1.TabStop = false;
+        // 
+        // pictureBox2
+        // 
+        pictureBox2.Image = PawfectCareLimited_Winforms_.Resource1.usernamePaw;
+        pictureBox2.Location = new Point(16, 136);
+        pictureBox2.Name = "pictureBox2";
+        pictureBox2.Size = new Size(22, 23);
+        pictureBox2.TabIndex = 1;
+        pictureBox2.TabStop = false;
+        // 
+        // pictureBox3
+        // 
+        pictureBox3.Image = PawfectCareLimited_Winforms_.Resource1.emailPaw;
+        pictureBox3.Location = new Point(16, 187);
+        pictureBox3.Name = "pictureBox3";
+        pictureBox3.Size = new Size(22, 17);
+        pictureBox3.TabIndex = 2;
+        pictureBox3.TabStop = false;
+        // 
+        // pictureBox4
+        // 
+        pictureBox4.Image = PawfectCareLimited_Winforms_.Resource1.passwordPAw;
+        pictureBox4.Location = new Point(16, 229);
+        pictureBox4.Name = "pictureBox4";
+        pictureBox4.Size = new Size(22, 24);
+        pictureBox4.TabIndex = 3;
+        pictureBox4.TabStop = false;
+        // 
+        // pictureBox5
+        // 
+        pictureBox5.Image = PawfectCareLimited_Winforms_.Resource1.passwordPAw;
+        pictureBox5.Location = new Point(16, 282);
+        pictureBox5.Name = "pictureBox5";
+        pictureBox5.Size = new Size(22, 24);
+        pictureBox5.TabIndex = 4;
+        pictureBox5.TabStop = false;
+        // 
+        // linkLabel1
+        // 
+        linkLabel1.AutoSize = true;
+        linkLabel1.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        linkLabel1.LinkColor = Color.Teal;
+        linkLabel1.Location = new Point(203, 320);
+        linkLabel1.Name = "linkLabel1";
+        linkLabel1.Size = new Size(73, 14);
+        linkLabel1.TabIndex = 7;
+        linkLabel1.TabStop = true;
+        linkLabel1.Text = "Clear fields";
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Font = new Font("Britannic Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        label1.ForeColor = Color.DarkCyan;
+        label1.Location = new Point(105, 82);
+        label1.Name = "label1";
+        label1.Size = new Size(78, 21);
+        label1.TabIndex = 8;
+        label1.Text = "SIGN UP";
+        // 
+        // button1
+        // 
+        button1.BackColor = Color.DarkCyan;
+        button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+        button1.ForeColor = Color.White;
+        button1.Location = new Point(80, 356);
+        button1.Name = "button1";
+        button1.Size = new Size(131, 34);
+        button1.TabIndex = 9;
+        button1.Text = "SIGN UP";
+        button1.UseVisualStyleBackColor = false;
+        // 
+        // linkLabel2
+        // 
+        linkLabel2.AutoSize = true;
+        linkLabel2.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        linkLabel2.LinkColor = Color.Teal;
+        linkLabel2.Location = new Point(129, 393);
+        linkLabel2.Name = "linkLabel2";
+        linkLabel2.Size = new Size(34, 14);
+        linkLabel2.TabIndex = 10;
+        linkLabel2.TabStop = true;
+        linkLabel2.Text = "EXIT";
+        // 
+        // panel1
+        // 
+        panel1.BackColor = Color.DarkCyan;
+        panel1.Location = new Point(16, 165);
+        panel1.Name = "panel1";
+        panel1.Size = new Size(260, 1);
+        panel1.TabIndex = 11;
+        // 
+        // panel2
+        // 
+        panel2.BackColor = Color.DarkCyan;
+        panel2.Location = new Point(16, 210);
+        panel2.Name = "panel2";
+        panel2.Size = new Size(260, 1);
+        panel2.TabIndex = 12;
+        // 
+        // panel3
+        // 
+        panel3.BackColor = Color.DarkCyan;
+        panel3.Location = new Point(16, 259);
+        panel3.Name = "panel3";
+        panel3.Size = new Size(260, 1);
+        panel3.TabIndex = 5;
+        // 
+        // panel4
+        // 
+        panel4.BackColor = Color.DarkCyan;
+        panel4.Location = new Point(18, 310);
+        panel4.Name = "panel4";
+        panel4.Size = new Size(260, 1);
+        panel4.TabIndex = 5;
+        // 
+        // textBox1
+        // 
+        textBox1.ForeColor = SystemColors.WindowFrame;
+        textBox1.Location = new Point(38, 136);
+        textBox1.Name = "textBox1";
+        textBox1.Size = new Size(238, 23);
+        textBox1.TabIndex = 13;
+        // 
+        // textBox2
+        // 
+        textBox2.ForeColor = SystemColors.WindowFrame;
+        textBox2.Location = new Point(38, 181);
+        textBox2.Name = "textBox2";
+        textBox2.Size = new Size(238, 23);
+        textBox2.TabIndex = 14;
+        // 
+        // textBox3
+        // 
+        textBox3.ForeColor = SystemColors.WindowFrame;
+        textBox3.Location = new Point(38, 229);
+        textBox3.Name = "textBox3";
+        textBox3.Size = new Size(238, 23);
+        textBox3.TabIndex = 15;
+        // 
+        // textBox4
+        // 
+        textBox4.ForeColor = SystemColors.WindowFrame;
+        textBox4.Location = new Point(38, 282);
+        textBox4.Name = "textBox4";
+        textBox4.Size = new Size(238, 23);
+        textBox4.TabIndex = 16;
+        // 
+        // SignUpForms
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = Color.White;
+        ClientSize = new Size(290, 436);
+        Controls.Add(textBox4);
+        Controls.Add(textBox3);
+        Controls.Add(textBox2);
+        Controls.Add(textBox1);
+        Controls.Add(panel4);
+        Controls.Add(panel3);
+        Controls.Add(panel2);
+        Controls.Add(panel1);
+        Controls.Add(linkLabel2);
+        Controls.Add(button1);
+        Controls.Add(label1);
+        Controls.Add(linkLabel1);
+        Controls.Add(pictureBox5);
+        Controls.Add(pictureBox4);
+        Controls.Add(pictureBox3);
+        Controls.Add(pictureBox2);
+        Controls.Add(pictureBox1);
+        FormBorderStyle = FormBorderStyle.None;
+        Name = "SignUpForms";
+        Text = "SignUpForms";
+        Load += SignUpForms_Load;
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private PictureBox pictureBox1;
+    private PictureBox pictureBox2;
+    private PictureBox pictureBox3;
+    private PictureBox pictureBox4;
+    private PictureBox pictureBox5;
+    private LinkLabel linkLabel1;
+    private Label label1;
+    private Button button1;
+    private LinkLabel linkLabel2;
+    private Panel panel1;
+    private Panel panel2;
+    private Panel panel3;
+    private Panel panel4;
+    private TextBox textBox1;
+    private TextBox textBox2;
+    private TextBox textBox3;
+    private TextBox textBox4;
+}
+}
---
+++ PawfectCareLimited/PawfectCareLimited/SignUpForms/SignUpForms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PawfectCareLimited(Winforms).SignUpForms
+{
+    public partial class SignUpForms : Form
+{
+    public SignUpForms()
+    {
+        InitializeComponent();
+    }
+
+    private void SignUpForms_Load(object sender, EventArgs e)
+    {
+
+    }
+}
+}
