606e17c33475d368ebd2b4bb6b0fe3653f9d7021
Nahida_22 <nr681@live.mdx.ac.uk>
2025-04-16 01:35:22 +0400

    LocationInterfaceForm

---
+++ PawfectCareLimited/PawfectCareLimited/LocationForm/LocationTableInterface.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace PawfectCareLimited
+{
+    partial class LocationTableInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            locationLabel = new Label();
+            locationTableDataGridView = new DataGridView();
+            locationUpdateButton = new Button();
+            locationDeleteButton = new Button();
+            SearchBarTextBox = new TextBox();
+            SearchButton = new Button();
+            SearchFieldComboBox = new ComboBox();
+            viewAllButton = new Button();
+            locationInsertButton = new Button();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)locationTableDataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // locationLabel
+            // 
+            locationLabel.AutoSize = true;
+            locationLabel.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            locationLabel.ForeColor = Color.DarkCyan;
+            locationLabel.Location = new Point(242, 18);
+            locationLabel.Name = "locationLabel";
+            locationLabel.Size = new Size(230, 29);
+            locationLabel.TabIndex = 0;
+            locationLabel.Text = "Location Table";
+            locationLabel.Click += locationLabel_Click;
+            // 
+            // locationTableDataGridView
+            // 
+            locationTableDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            locationTableDataGridView.Location = new Point(12, 121);
+            locationTableDataGridView.Margin = new Padding(3, 2, 3, 2);
+            locationTableDataGridView.Name = "locationTableDataGridView";
+            locationTableDataGridView.RowHeadersWidth = 51;
+            locationTableDataGridView.Size = new Size(676, 304);
+            locationTableDataGridView.TabIndex = 1;
+            // 
+            // locationUpdateButton
+            // 
+            locationUpdateButton.BackColor = Color.Orange;
+            locationUpdateButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            locationUpdateButton.ForeColor = Color.White;
+            locationUpdateButton.Location = new Point(288, 455);
+            locationUpdateButton.Margin = new Padding(3, 2, 3, 2);
+            locationUpdateButton.Name = "locationUpdateButton";
+            locationUpdateButton.Size = new Size(106, 43);
+            locationUpdateButton.TabIndex = 3;
+            locationUpdateButton.Text = "UPDATE";
+            locationUpdateButton.UseVisualStyleBackColor = false;
+            locationUpdateButton.Click += locationUpdateButton_Click;
+            // 
+            // locationDeleteButton
+            // 
+            locationDeleteButton.BackColor = Color.IndianRed;
+            locationDeleteButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            locationDeleteButton.ForeColor = Color.White;
+            locationDeleteButton.Location = new Point(176, 455);
+            locationDeleteButton.Margin = new Padding(3, 2, 3, 2);
+            locationDeleteButton.Name = "locationDeleteButton";
+            locationDeleteButton.Size = new Size(106, 43);
+            locationDeleteButton.TabIndex = 4;
+            locationDeleteButton.Text = "DELETE";
+            locationDeleteButton.UseVisualStyleBackColor = false;
+            locationDeleteButton.Click += locationDeleteButton_Click;
+            // 
+            // SearchBarTextBox
+            // 
+            SearchBarTextBox.Location = new Point(139, 76);
+            SearchBarTextBox.Margin = new Padding(3, 2, 3, 2);
+            SearchBarTextBox.Name = "SearchBarTextBox";
+            SearchBarTextBox.Size = new Size(306, 23);
+            SearchBarTextBox.TabIndex = 5;
+            // 
+            // SearchButton
+            // 
+            SearchButton.BackColor = Color.DarkCyan;
+            SearchButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            SearchButton.ForeColor = Color.White;
+            SearchButton.Location = new Point(451, 66);
+            SearchButton.Margin = new Padding(3, 2, 3, 2);
+            SearchButton.Name = "SearchButton";
+            SearchButton.Size = new Size(96, 39);
+            SearchButton.TabIndex = 6;
+            SearchButton.Text = "SEARCH";
+            SearchButton.UseVisualStyleBackColor = false;
+            SearchButton.Click += SearchButton_Click;
+            // 
+            // SearchFieldComboBox
+            // 
+            SearchFieldComboBox.FormattingEnabled = true;
+            SearchFieldComboBox.Items.AddRange(new object[] { "LocationID", "Name", "Address", "Phone", "Email"});
+            SearchFieldComboBox.Location = new Point(553, 76);
+            SearchFieldComboBox.Margin = new Padding(3, 2, 3, 2);
+            SearchFieldComboBox.Name = "SearchFieldComboBox";
+            SearchFieldComboBox.Size = new Size(133, 23);
+            SearchFieldComboBox.TabIndex = 7;
+            // 
+            // viewAllButton
+            // 
+            viewAllButton.BackColor = Color.DarkCyan;
+            viewAllButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            viewAllButton.ForeColor = Color.White;
+            viewAllButton.Location = new Point(601, 429);
+            viewAllButton.Margin = new Padding(3, 2, 3, 2);
+            viewAllButton.Name = "viewAllButton";
+            viewAllButton.Size = new Size(87, 30);
+            viewAllButton.TabIndex = 8;
+            viewAllButton.Text = "View All";
+            viewAllButton.UseVisualStyleBackColor = false;
+            viewAllButton.Click += viewAllButton_Click;
+            // 
+            // locationInsertButton
+            // 
+            locationInsertButton.BackColor = Color.ForestGreen;
+            locationInsertButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            locationInsertButton.ForeColor = Color.White;
+            locationInsertButton.Location = new Point(400, 455);
+            locationInsertButton.Margin = new Padding(3, 2, 3, 2);
+            locationInsertButton.Name = "locationInsertButton";
+            locationInsertButton.Size = new Size(106, 43);
+            locationInsertButton.TabIndex = 9;
+            locationInsertButton.Text = "INSERT";
+            locationInsertButton.UseVisualStyleBackColor = false;
+            locationInsertButton.Click += locationInsertButton_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkCyan;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(12, 509);
+            button1.Name = "button1";
+            button1.Size = new Size(76, 43);
+            button1.TabIndex = 10;
+            button1.Text = "BACK";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = PawfectCareLimited_Winforms_.Resource1.logoPawfectCare;
+            pictureBox1.Location = new Point(-1, -9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(134, 114);
+            pictureBox1.TabIndex = 25;
+            pictureBox1.TabStop = false;
+            // 
+            // LocationTableInterface
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(700, 564);
+            Controls.Add(button1);
+            Controls.Add(locationInsertButton);
+            Controls.Add(viewAllButton);
+            Controls.Add(SearchFieldComboBox);
+            Controls.Add(SearchButton);
+            Controls.Add(SearchBarTextBox);
+            Controls.Add(locationDeleteButton);
+            Controls.Add(locationUpdateButton);
+            Controls.Add(locationTableDataGridView);
+            Controls.Add(locationLabel);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "LocationTableInterface";
+            Text = "LocationTableInterface";
+            ((System.ComponentModel.ISupportInitialize)locationTableDataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label locationLabel;
+        private DataGridView locationTableDataGridView;
+        private Button locationUpdateButton;
+        private Button locationDeleteButton;
+        private TextBox SearchBarTextBox;
+        private Button SearchButton;
+        private ComboBox SearchFieldComboBox;
+        private Button viewAllButton;
+        private Button locationInsertButton;
+        private Button button1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ PawfectCareLimited/PawfectCareLimited/LocationForm/LocationTableInterface.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace PawfectCareLimited
+{
+    public partial class LocationTableInterface : Form
+{
+        // Initialise an instance of HttpClient for API calls.
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        // Constructor.
+        public LocationTableInterface()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(this.LocationTableInterface_Load);
+        }
+
+
+        // Method to initialise the Owner Windoe.
+        private async void LocationTableInterface_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Call LoadLocations 
+                await Task.Run(() => LoadLocations());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading data: {ex.Message}");
+            }
+        }
+
+        private async Task LoadLocations()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string apiUrl = "https://localhost:7038/api/Location/all";
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        ApiResponse apiResponse = JsonConvert.DeserializeObject<ApiResponse>(json);
+
+                        if (apiResponse.success)
+                        {
+                            // Binding to the grid
+                            locationTableDataGridView.Invoke(() =>
+                            {
+                                locationTableDataGridView.AutoGenerateColumns = true;
+                                locationTableDataGridView.DataSource = apiResponse.data;
+                            });
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error: " + apiResponse.message);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to fetch data. Status code: " + response.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Exception: " + ex.Message);
+                }
+            }
+        }
+
+        // Event Listener for when the UPDATE button is clicked.
+        private void locationUpdateButton_Click(object sender, EventArgs e)
+        {
+            // Check if a row is selected.
+            if (locationTableDataGridView.CurrentRow != null)
+            {
+                // Get values from selected row
+                string id = locationTableDataGridView.CurrentRow.Cells[0].Value?.ToString();
+                string Name = locationTableDataGridView.CurrentRow.Cells[2].Value?.ToString();
+                string Address = locationTableDataGridView.CurrentRow.Cells[3].Value?.ToString();
+                string Phone = locationTableDataGridView.CurrentRow.Cells[4].Value?.ToString();
+                string Email = locationTableDataGridView.CurrentRow.Cells[5].Value?.ToString();
+
+
+                // Call the UPDATE Window and pass the values of the selected row in its constructor.
+                var locationUpdateInterface = new LocationUpdateForm(id, Name, Address, Phone,Email);
+
+                // Subscribe to the event
+                locationUpdateInterface.LocationUpdated += (s, args) => LoadLocations();
+
+                // Show the window.
+                locationUpdateInterface.ShowDialog();
+            }
+            else
+            {
+                // Show error message.
+                MessageBox.Show("Please select a row to update.");
+            }
+        }
+
+        private async void locationDeleteButton_Click(object sender, EventArgs e)
+        {
+            // Confirm deletion
+            var confirmResult = MessageBox.Show("Are you sure to delete this branch?",
+                                                "Confirm Delete",
+                                                MessageBoxButtons.YesNo);
+            if (confirmResult != DialogResult.Yes)
+                return;
+
+            // Get the id of the selected row.
+            string id = locationTableDataGridView.CurrentRow.Cells[0].Value?.ToString();
+
+            // Check if the a row is selected.
+            if (string.IsNullOrEmpty(id))
+            {
+                MessageBox.Show("Please select a valid location.");
+                return;
+            }
+
+            // Initialise an instance of HttpClient for API calls.
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string baseUrl = "https://localhost:7038/api/location"; // Endpoint for deletion.
+                    string deleteUrl = $"{baseUrl}?locationId={id}";
+
+                    HttpResponseMessage response = await client.DeleteAsync(deleteUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Location deleted successfully.");
+
+                        // Refresh the table.
+                        await Task.Run(() => LoadLocations());
+                    }
+                    else
+                    {
+                        // Show error message.
+                        string error = await response.Content.ReadAsStringAsync();
+                        MessageBox.Show($"Failed to delete location. Server says: {error}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error during deletion: {ex.Message}");
+                }
+
+            }
+        }
+
+        private async void SearchButton_Click(object sender, EventArgs e)
+        {
+            string fieldName = SearchFieldComboBox.SelectedItem?.ToString();
+            string fieldValue = SearchBarTextBox.Text.Trim();
+
+            if (string.IsNullOrEmpty(fieldName) || string.IsNullOrEmpty(fieldValue))
+            {
+                MessageBox.Show("Please select a field and enter a value.");
+                return;
+            }
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string baseUrl = "https://localhost:7038/api/location";
+                    string fullUrl = $"{baseUrl}?fieldName={fieldName}&fieldValue={fieldValue}";
+
+                    HttpResponseMessage response = await client.GetAsync(fullUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<OperationResult>(json);
+
+                        if (result.success)
+                        {
+                            var dataList = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(result.data.ToString());
+
+                            DataTable dt = ConvertToDataTable(dataList);
+                            locationTableDataGridView.DataSource = dt;
+                        }
+                        else
+                        {
+                            MessageBox.Show(result.message);
+                            locationTableDataGridView.DataSource = null;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No such record value was found.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+
+        }
+
+        // Method to convert the search results in the form of a table.
+        public static DataTable ConvertToDataTable(List<Dictionary<string, object>> list)
+        {
+            DataTable table = new DataTable();
+
+            if (list == null || list.Count == 0)
+                return table;
+
+            // Create columns
+            foreach (var key in list[0].Keys)
+            {
+                table.Columns.Add(key);
+            }
+
+            // Add rows
+            foreach (var dict in list)
+            {
+                var row = table.NewRow();
+                foreach (var kvp in dict)
+                {
+                    if (kvp.Key == "ApptDate" && kvp.Value != null)
+                    {
+                        if (DateTime.TryParse(kvp.Value.ToString(), out DateTime date))
+                        {
+                            row[kvp.Key] = date;
+                        }
+                        else
+                        {
+                            row[kvp.Key] = DBNull.Value;
+                        }
+                    }
+                    else
+                    {
+                        row[kvp.Key] = kvp.Value ?? DBNull.Value;
+                    }
+                }
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+        private async void viewAllButton_Click(object sender, EventArgs e)
+        {
+            // Reset the table. 
+            await Task.Run(() => LoadLocations());
+        }
+
+        private void locationInsertButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void locationLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+   
+
+    public class Location
+    {
+        public string LocationID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        
+        public string Email { get; set; }
+     
+    }
+
+    public class LocationApiResponse
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+        public List<Location> data { get; set; }
+    }
+
+
+}
+
