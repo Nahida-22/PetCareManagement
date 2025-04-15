1e70703e18b9389a717ebb8385f36ae09279df2d
APJB7 <pothanah.adrian@gmail.com>
2025-04-15 11:31:27 +0400

    Adding images in resources folderfor the login part

--- PawfectCareLimited/PawfectCareLimited/LoginForms/loginForm.Designer.cs
+++ PawfectCareLimited/PawfectCareLimited/LoginForms/loginForm.Designer.cs
@@ -28,19 +28,48 @@
     /// </summary>
     private void InitializeComponent()
     {
+        pictureBox1 = new PictureBox();
+        label1 = new Label();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
         SuspendLayout();
+        // 
+        // pictureBox1
+        // 
+        pictureBox1.Image = PawfectCareLimited_Winforms_.Resource1.image_7;
+        pictureBox1.Location = new Point(87, -26);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new Size(118, 114);
+        pictureBox1.TabIndex = 0;
+        pictureBox1.TabStop = false;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(98, 94);
+        label1.Name = "label1";
+        label1.Size = new Size(42, 15);
+        label1.TabIndex = 1;
+        label1.Text = "LOGIN";
+        label1.Click += label1_Click;
         // 
         // loginForm
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(306, 443);
+        Controls.Add(label1);
+        Controls.Add(pictureBox1);
         Name = "loginForm";
         Text = "loginForm";
         Load += loginForm_Load;
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
         ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private PictureBox pictureBox1;
+    private Label label1;
 }
 }
--- PawfectCareLimited/PawfectCareLimited/Resource1.Designer.cs
+++ PawfectCareLimited/PawfectCareLimited/Resource1.Designer.cs
@@ -71,6 +71,16 @@ namespace PawfectCareLimited_Winforms_ {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap image_7 {
+            get {
+                object obj = ResourceManager.GetObject("image 7", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] logo {
