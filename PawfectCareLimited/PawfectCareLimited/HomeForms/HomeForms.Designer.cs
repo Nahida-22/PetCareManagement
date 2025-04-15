414d3b61205ac75e26296a1a69b61ff017bf3bcd
APJB7 <pothanah.adrian@gmail.com>
2025-04-15 21:50:33 +0400

    Added backend for the home page, partially finished (back up purposes)

--- PawfectCareLimited/PawfectCareLimited/HomeForms/HomeForms.Designer.cs
+++ PawfectCareLimited/PawfectCareLimited/HomeForms/HomeForms.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             pictureBox1 = new PictureBox();
+            greetingLabel = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -42,22 +47,91 @@
             pictureBox1.TabIndex = 26;
             pictureBox1.TabStop = false;
             // 
+            // greetingLabel
+            // 
+            greetingLabel.AutoSize = true;
+            greetingLabel.BackColor = Color.DimGray;
+            greetingLabel.ForeColor = Color.White;
+            greetingLabel.Location = new Point(269, 142);
+            greetingLabel.Name = "greetingLabel";
+            greetingLabel.Size = new Size(19, 15);
+            greetingLabel.TabIndex = 27;
+            greetingLabel.Text = "    ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.DimGray;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(269, 184);
+            label2.Name = "label2";
+            label2.Size = new Size(19, 15);
+            label2.TabIndex = 29;
+            label2.Text = "    ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.DimGray;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(269, 224);
+            label3.Name = "label3";
+            label3.Size = new Size(19, 15);
+            label3.TabIndex = 30;
+            label3.Text = "    ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Britannic Bold", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DarkCyan;
+            label1.Location = new Point(233, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 32);
+            label1.TabIndex = 31;
+            label1.Text = "HOME";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Brown;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(233, 284);
+            button1.Name = "button1";
+            button1.Size = new Size(92, 46);
+            button1.TabIndex = 32;
+            button1.Text = "LOG OUT";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
             // HomeForms
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(586, 367);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(greetingLabel);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "HomeForms";
             Text = "HomeForms";
+            Load += HomeForms_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private Label greetingLabel;
+        private Label label2;
+        private Label label3;
+        private Label label1;
+        private Button button1;
     }
 }
--- PawfectCareLimited/PawfectCareLimited/HomeForms/HomeForms.cs
+++ PawfectCareLimited/PawfectCareLimited/HomeForms/HomeForms.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,10 +12,29 @@ using System.Windows.Forms;
 namespace PawfectCareLimited
 {
     public partial class HomeForms : Form
-{
-    public HomeForms()
     {
-        InitializeComponent();
+        private string currentUsername;
+        private string currentEmail;
+
+        // Constructor that receives username and email
+        public HomeForms(string username, string email)
+        {
+            InitializeComponent();
+            currentUsername = username;
+            currentEmail = email;
+        }
+
+        private void HomeForms_Load(object sender, EventArgs e)
+        {
+            // Ensure you have these labels added in the Designer
+            greetingLabel.Text = $"Hello, {currentUsername}!";
+            label2.Text = $"Email: {currentEmail}";
+            label3.Text = $"Username: {currentUsername}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
-}
 }
