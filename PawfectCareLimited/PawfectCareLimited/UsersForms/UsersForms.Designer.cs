91f6eed7cf9d813e4f5d0afa95173d101598d5e4
APJB7 <pothanah.adrian@gmail.com>
2025-04-15 21:05:03 +0400

    Modified some front end and added the usersForms as well as its backend to read from the text file

--- PawfectCareLimited/PawfectCareLimited/UsersForms/UsersForms.Designer.cs
+++ PawfectCareLimited/PawfectCareLimited/UsersForms/UsersForms.Designer.cs
@@ -20,20 +20,87 @@
         base.Dispose(disposing);
     }
 
-    #region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "UsersForms";
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            apppointmentUpdateLabel = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = PawfectCareLimited_Winforms_.Resource1.logoPawfectCare;
+            pictureBox1.Location = new Point(0, -2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(134, 114);
+            pictureBox1.TabIndex = 25;
+            pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(152, 66);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(511, 361);
+            dataGridView1.TabIndex = 26;
+            // 
+            // apppointmentUpdateLabel
+            // 
+            apppointmentUpdateLabel.AutoSize = true;
+            apppointmentUpdateLabel.Font = new Font("Arial Rounded MT Bold", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            apppointmentUpdateLabel.ForeColor = Color.DarkCyan;
+            apppointmentUpdateLabel.Location = new Point(355, 23);
+            apppointmentUpdateLabel.Name = "apppointmentUpdateLabel";
+            apppointmentUpdateLabel.Size = new Size(95, 28);
+            apppointmentUpdateLabel.TabIndex = 27;
+            apppointmentUpdateLabel.Text = "USERS";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkCyan;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(12, 436);
+            button1.Name = "button1";
+            button1.Size = new Size(76, 43);
+            button1.TabIndex = 28;
+            button1.Text = "BACK";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // UsersForms
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 491);
+            Controls.Add(button1);
+            Controls.Add(apppointmentUpdateLabel);
+            Controls.Add(dataGridView1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "UsersForms";
+            Text = "UsersForms";
+            Load += UsersForms_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private DataGridView dataGridView1;
+        private Label apppointmentUpdateLabel;
+        private Button button1;
     }
-
-    #endregion
-}
 }
--- PawfectCareLimited/PawfectCareLimited/UsersForms/UsersForms.cs
+++ PawfectCareLimited/PawfectCareLimited/UsersForms/UsersForms.cs
@@ -1,20 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace PawfectCareLimited
 {
     public partial class UsersForms : Form
-{
-    public UsersForms()
     {
-        InitializeComponent();
+        public UsersForms()
+        {
+            InitializeComponent();
+        }
+
+        private void UsersForms_Load(object sender, EventArgs e)
+        {
+            // Load the users and display them in the DataGridView in the UserForms.Designer.cs
+            LoadUsers();
+        }
+
+        private void LoadUsers()
+        {
+            string filePath = "users.txt";
+
+            // Check if the users.txt file exists
+            if (File.Exists(filePath))
+            {
+                // Create a list to store the user data
+                List<User> users = new List<User>();
+
+                // Read all lines from the file "users.txt"
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    // Split each line by ":" to get username and password
+                    string[] userParts = line.Split(':');
+                    if (userParts.Length == 2)
+                    {
+                        // Create a User object and add it to the list
+                        users.Add(new User
+                        {
+                            Username = userParts[0],
+                            Password = userParts[1]
+                        });
+                    }
+                }
+
+                // Bind the list of users to the DataGridView
+                dataGridView1.DataSource = users;
+            }
+            else
+            {
+                // Show an error message if the users.txt file does not exist
+                MessageBox.Show("No users found.", "File Missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Define a simple User class to store user data
+        public class User
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Close the current form and return to the previous form
+            this.Close();
+        }
     }
-}
 }
