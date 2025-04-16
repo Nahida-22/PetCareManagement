2b4de459582210fc252da86c979b6ead404bf354
APJB7 <pothanah.adrian@gmail.com>
2025-04-16 11:36:10 +0400

    Added front end to OrderInsertForm.cs

--- PawfectCareLimited/PawfectCareLimited/OrderForms/OrderInsertForm.Designer.cs
+++ PawfectCareLimited/PawfectCareLimited/OrderForms/OrderInsertForm.Designer.cs
@@ -40,113 +40,161 @@
             expiryDateLabel = new Label();
             orderDateValue = new DateTimePicker();
             insertButton = new Button();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 16F);
-            label1.Location = new Point(12, 9);
+            label1.Font = new Font("Arial Rounded MT Bold", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DarkCyan;
+            label1.Location = new Point(218, 32);
             label1.Name = "label1";
-            label1.Size = new Size(319, 37);
+            label1.Size = new Size(273, 24);
             label1.TabIndex = 0;
             label1.Text = "Enter a new Order Record";
             // 
             // medicationIdLabel
             // 
             medicationIdLabel.AutoSize = true;
-            medicationIdLabel.Location = new Point(12, 78);
+            medicationIdLabel.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            medicationIdLabel.ForeColor = SystemColors.ControlDarkDark;
+            medicationIdLabel.Location = new Point(177, 83);
             medicationIdLabel.Name = "medicationIdLabel";
-            medicationIdLabel.Size = new Size(73, 20);
+            medicationIdLabel.Size = new Size(69, 17);
             medicationIdLabel.TabIndex = 1;
             medicationIdLabel.Text = "Order ID :";
             // 
             // medicationNameLabel
             // 
             medicationNameLabel.AutoSize = true;
-            medicationNameLabel.Location = new Point(12, 128);
+            medicationNameLabel.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            medicationNameLabel.ForeColor = SystemColors.ControlDarkDark;
+            medicationNameLabel.Location = new Point(177, 121);
             medicationNameLabel.Name = "medicationNameLabel";
-            medicationNameLabel.Size = new Size(110, 20);
+            medicationNameLabel.Size = new Size(103, 17);
             medicationNameLabel.TabIndex = 2;
             medicationNameLabel.Text = "Medication ID :";
             // 
             // orderIdValue
             // 
-            orderIdValue.Location = new Point(164, 78);
+            orderIdValue.Location = new Point(311, 83);
+            orderIdValue.Margin = new Padding(3, 2, 3, 2);
             orderIdValue.Name = "orderIdValue";
-            orderIdValue.Size = new Size(193, 27);
+            orderIdValue.Size = new Size(219, 23);
             orderIdValue.TabIndex = 3;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 174);
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ControlDarkDark;
+            label2.Location = new Point(177, 155);
             label2.Name = "label2";
-            label2.Size = new Size(72, 20);
+            label2.Size = new Size(70, 17);
             label2.TabIndex = 4;
             label2.Text = "Quantity :";
             // 
             // medicationIdValue
             // 
-            medicationIdValue.Location = new Point(164, 128);
+            medicationIdValue.Location = new Point(311, 121);
+            medicationIdValue.Margin = new Padding(3, 2, 3, 2);
             medicationIdValue.Name = "medicationIdValue";
-            medicationIdValue.Size = new Size(193, 27);
+            medicationIdValue.Size = new Size(219, 23);
             medicationIdValue.TabIndex = 5;
             // 
             // quantityValue
             // 
-            quantityValue.Location = new Point(164, 171);
+            quantityValue.Location = new Point(311, 153);
+            quantityValue.Margin = new Padding(3, 2, 3, 2);
             quantityValue.Name = "quantityValue";
-            quantityValue.Size = new Size(193, 27);
+            quantityValue.Size = new Size(219, 23);
             quantityValue.TabIndex = 6;
             // 
             // stockQuantityLabel
             // 
             stockQuantityLabel.AutoSize = true;
-            stockQuantityLabel.Location = new Point(12, 219);
+            stockQuantityLabel.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            stockQuantityLabel.ForeColor = SystemColors.ControlDarkDark;
+            stockQuantityLabel.Location = new Point(177, 189);
             stockQuantityLabel.Name = "stockQuantityLabel";
-            stockQuantityLabel.Size = new Size(98, 20);
+            stockQuantityLabel.Size = new Size(93, 17);
             stockQuantityLabel.TabIndex = 7;
             stockQuantityLabel.Text = "Order Status :";
             // 
             // statusValue
             // 
-            statusValue.Location = new Point(164, 216);
+            statusValue.Location = new Point(311, 187);
+            statusValue.Margin = new Padding(3, 2, 3, 2);
             statusValue.Name = "statusValue";
-            statusValue.Size = new Size(193, 27);
+            statusValue.Size = new Size(219, 23);
             statusValue.TabIndex = 8;
             // 
             // expiryDateLabel
             // 
             expiryDateLabel.AutoSize = true;
-            expiryDateLabel.Location = new Point(16, 265);
+            expiryDateLabel.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            expiryDateLabel.ForeColor = SystemColors.ControlDarkDark;
+            expiryDateLabel.Location = new Point(181, 224);
             expiryDateLabel.Name = "expiryDateLabel";
-            expiryDateLabel.Size = new Size(94, 20);
+            expiryDateLabel.Size = new Size(88, 17);
             expiryDateLabel.TabIndex = 13;
             expiryDateLabel.Text = "Order Date : ";
             // 
             // orderDateValue
             // 
-            orderDateValue.Location = new Point(164, 265);
+            orderDateValue.Location = new Point(311, 224);
+            orderDateValue.Margin = new Padding(3, 2, 3, 2);
             orderDateValue.Name = "orderDateValue";
-            orderDateValue.Size = new Size(250, 27);
+            orderDateValue.Size = new Size(219, 23);
             orderDateValue.TabIndex = 14;
             // 
             // insertButton
             // 
-            insertButton.Location = new Point(624, 380);
+            insertButton.BackColor = Color.ForestGreen;
+            insertButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            insertButton.ForeColor = Color.White;
+            insertButton.Location = new Point(290, 278);
+            insertButton.Margin = new Padding(3, 2, 3, 2);
             insertButton.Name = "insertButton";
-            insertButton.Size = new Size(127, 29);
+            insertButton.Size = new Size(117, 49);
             insertButton.TabIndex = 15;
             insertButton.Text = "Insert Record";
-            insertButton.UseVisualStyleBackColor = true;
+            insertButton.UseVisualStyleBackColor = false;
             insertButton.Click += insertButton_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = PawfectCareLimited_Winforms_.Resource1.logoPawfectCare;
+            pictureBox1.Location = new Point(-2, -2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(119, 108);
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkCyan;
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(12, 284);
+            button2.Name = "button2";
+            button2.Size = new Size(76, 43);
+            button2.TabIndex = 35;
+            button2.Text = "BACK";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // OrderInsertForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.White;
+            ClientSize = new Size(700, 338);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
             Controls.Add(insertButton);
             Controls.Add(orderDateValue);
             Controls.Add(expiryDateLabel);
@@ -159,8 +207,11 @@
             Controls.Add(medicationNameLabel);
             Controls.Add(medicationIdLabel);
             Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "OrderInsertForm";
             Text = "MedicationInsertForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -179,5 +230,7 @@
     private Label expiryDateLabel;
     private DateTimePicker orderDateValue;
     private Button insertButton;
-}
+        private PictureBox pictureBox1;
+        private Button button2;
+    }
 }
